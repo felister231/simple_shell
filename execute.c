e6d1199d1c297cb6c79ff9235ea8c4fc24c4f52e
Victorchiemeka <Victorchukwuemeka127@gmail.com>
2023-07-16 22:32:47 +0100

    second implementation of the project

---
+++ execute.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+void execute(char *input_command)
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		free(input_command);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execlp(input_command, input_command, NULL);
+
+		perror("execlp");
+		free (input_command);
+		exit(EXIT_FAILURE);
+	}
+}
