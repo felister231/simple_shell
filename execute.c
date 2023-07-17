fa7dab4bfa3f8eefd39ba7562e6efdd87e8ac690
felister231 <felimu2030@gmail.com>
2023-07-17 08:32:03 -0700

    execute

--- execute.c
+++ execute.c
@@ -2,7 +2,6 @@
 
 void execute(char *input_command)
 {
-	int child_process, exit_process, number_signal;
 	pid_t pid = fork();
 
 	if (pid == -1)
@@ -18,25 +17,6 @@ void execute(char *input_command)
 		perror("execlp");
 		free (input_command);
 		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		waitpid(pid, &child_process, 0);
-		if (WIFEXITED(child_process))
-		{
-			exit_process = WEXITSTATUS(child_process);
-			if (exit_process != 0)
-			{
-				printf("Command exec failed with status %d\n",exit_process);
-			}
-		}
-		else if(WIFSIGNALED(child_process))
-		{
-			number_signal = WTERMSIG(child_process);
-			printf("Command terminated by number signal  %d\n", number_signal);
-
-		}
-		free(input_command);
 	}
 }
 int main()
