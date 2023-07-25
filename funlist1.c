858d13ddda252fb1ad8080ac7e992b0f53d66d2f
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-07-25 12:16:22 +0100

    fixed betty

--- funlist1.c
+++ funlist1.c
@@ -2,46 +2,46 @@
 
 /**
  * get_node_index - Get the index of a node in a linked list
- * @head: Pointer to the head of the linked list
- * @node: Pointer to the node whose index is to be found
+ * @head_pointer: Pointer to the head of the linked list
+ * @first_node: Pointer to the node whose index is to be found
  *
  * Returns the index of  given node in  linked list starting from the head.
  * Return: The index of the node in the list, or -1 if the node is not found
  */
 
-ssize_t get_node_index(list_t *head, list_t *node)
+ssize_t get_node_index(list_t *head_pointer, list_t *first_node)
 {
-	size_t i = 0;
+	size_t j = 0;
 
-	while (head)
+	while (head_pointer)
 	{
-		if (head == node)
-			return (i);
-		head = head->next;
-		i++;
+		if (head_pointer == first_node)
+			return (j);
+		head_pointer = head_pointer->next;
+		j++;
 	}
 	return (-1);
 }
 
 /**
  * node_starts_with - Find a node with a string that starts with  given prefix
- * @node: Pointer to the head of the linked list
- * @prefix: The prefix to search for at the beginning of the strings
- * @c: Optional character to check after the prefix, or -1 to skip this check
+ * @first_node: Pointer to the head of the linked list
+ * @affix: The prefix to search for at the beginning of the strings
+ * @d: Optional character to check after the prefix, or -1 to skip this check
  *
  * Return: Pointer to  first node with a matching string, or NULL if not found
  */
 
-list_t *node_starts_with(list_t *node, char *prefix, char c)
+list_t *node_starts_with(list_t *first_node, char *affix, char d)
 {
-	char *p = NULL;
+	char *pointer = NULL;
 
-	while (node)
+	while (first_node)
 	{
-		p = starts_with(node->str, prefix);
-		if (p && ((c == -1) || (*p == c)))
-			return (node);
-		node = node->next;
+		pointer = starts_with(first_node->str, affix);
+		if (pointer && ((d == -1) || (*pointer == d)))
+			return (first_node);
+		first_node = first_node->next;
 	}
 	return (NULL);
 }
@@ -49,44 +49,44 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 
 /**
  * print_list - Print the elements of a linked list
- * @h: Pointer to the head of the linked list
+ * @head: Pointer to the head of the linked list
  *
  * Return: The number of nodes in the list
  */
 
-size_t print_list(const list_t *h)
+size_t print_list(const list_t *head)
 {
-	size_t i = 0;
+	size_t j = 0;
 
-	while (h)
+	while (head)
 	{
-		_puts(convert_number(h->num, 10, 0));
+		_puts(convert_number(head->num, 10, 0));
 		_putchar(':');
 		_putchar(' ');
-		_puts(h->str ? h->str : "(nil)");
+		_puts(head->str ? head->str : "(nil)");
 		_puts("\n");
-		h = h->next;
-		i++;
+		head = head->next;
+		j++;
 	}
-	return (i);
+	return (j);
 }
 
 /**
  * list_to_strings - Convert a linked list to an array of strings
- * @head: Pointer to the head of the linked list
+ * @pointer_head: Pointer to the head of the linked list
  *
  * Return: An array of strings containing the elements of the linked list
  *         or NULL if memory allocation fails
  */
 
-char **list_to_strings(list_t *head)
+char **list_to_strings(list_t *pointer_head)
 {
-	list_t *node = head;
-	size_t i = list_len(head), j;
+	list_t *node = pointer_head;
+	size_t i = list_len(pointer_head), j;
 	char **strs;
 	char *str;
 
-	if (!head || !i)
+	if (!pointer_head || !i)
 		return (NULL);
 	strs = malloc(sizeof(char *) * (i + 1));
 	if (!strs)
@@ -111,19 +111,19 @@ char **list_to_strings(list_t *head)
 
 /**
  * list_len - Get the number of elements in a linked list
- * @h: Pointer to the head of the linked list
+ * @head: Pointer to the head of the linked list
  *
  * Return: The number of nodes in the linked list
  */
 
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *head)
 {
-	size_t i = 0;
+	size_t j = 0;
 
-	while (h)
+	while (head)
 	{
-		h = h->next;
-		i++;
+		head = head->next;
+		j++;
 	}
-	return (i);
+	return (j);
 }
